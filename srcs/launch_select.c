aec11665c18c1561f8aff5915ca883044a642daf
XD-OB <ob-96@hotmail.com>
2020-01-13 16:54:07 -0800

    finish the bonus select in rep

--- srcs/launch_select.c
+++ srcs/launch_select.c
@@ -56,22 +56,25 @@ static void			press_tab(t_select *select)
 	draw_state(select);
 }
 
-char				*
 
-void				enter_rep(t_select *select)
+static char			*full_path(char *parent, char *name)
 {
-	struct dirent	*dirent;
-	t_dlist			*node;
-	t_arg			*arg;
-	char			*path;
-	char			*parent;
-	DIR				*dir;
+	char			*res;
+	char			*tmp;
 
-	arg = (t_arg*)select->current->content;
-	dir = opendir(arg->str);
-	if (!dir)
-		return ;
-	parent = ft_strdup(arg->str);
+	if (parent[ft_strlen(parent) - 2] == '/')
+		res = ft_strjoin(parent, name);
+	else
+	{
+		tmp = ft_strjoin(parent, "/");
+		res = ft_strjoin(tmp, name);
+		free(tmp);
+	}
+	return (res);
+}
+
+static void			empty_select(t_select *select)
+{
 	if (select->lens_cols)
 		free(select->lens_cols);
 	if (select->args)
@@ -81,18 +84,47 @@ void				enter_rep(t_select *select)
 	select->nbr_cols = 0;
 	select->len_search = 0;
 	select->nbr_args = 0;
-	dirent = readdir(dir);
-	while (dirent)
+}
+
+static void			add_element(struct dirent *dirent,
+					t_select *select, char *parent)
+{
+	t_dlist			*node;
+	char			*path;
+	t_arg			*arg;
+
+	if (ft_strcmp(dirent->d_name, "..") &&
+		ft_strcmp(dirent->d_name, "."))
 	{
-		path = full_path(parent, dirent->d_name)
+		path = full_path(parent, dirent->d_name);
 		arg = create_arg(path);
 		free(path);
 		node = dct_lstnew_sm(arg, sizeof(arg));
 		dct_lstadd_last(&(select->args), node);
 		select->nbr_args++;
+	}
+}
+
+void				enter_rep(t_select *select)
+{
+	struct dirent	*dirent;
+	char			*parent;
+	DIR				*dir;
+
+	arg = (t_arg*)select->current->content;
+	dir = opendir(arg->str);
+	if (!dir)
+		return ;
+	parent = ft_strdup(arg->str);
+	empty_select(select);
+	dirent = readdir(dir);
+	while (dirent)
+	{
+		add_element(dirent, select, parent);
 		dirent = readdir(dir);
 	}
 	free(parent);
+	closedir(dir);
 	select->current = select->args;
 	draw_state(select);
 }
